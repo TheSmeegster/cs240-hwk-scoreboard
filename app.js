b934b3589ef6935050b55bae37b01ad392a4d196
Aidan Thiele <a.engelthiele@gmail.com>
2021-10-14 21:02:01 -0700

    Finished control panel aesthetics

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+function playMusic(){
+    let piano = new Audio('sounds/charge1.mp3')
+    piano.play();
+}
 
+function playPiano(){
+    console.log("piano");
+    let piano = new Audio('sounds/buildup.mp3')
+    piano.play();
+}
